ee5c0acbcb0799bd3441b3d81f6df96d215a6e29
gifgaf <gifgaf@gmail.com>
2016-11-25 03:01:34 +0300

    menu

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ app.set('port', (process.env.PORT || 3000));
 app.use('/', express.static(__dirname));
 
 app.listen(app.get('port'), function() {
-  console.log('Server started: http://localhost:' + app.get('port') + '/');
+  console.log('Server started:' + app.get('port') + '/');
 });
